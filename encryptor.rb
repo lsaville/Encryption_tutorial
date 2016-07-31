2234761ecd6e5e8228587f4be949ed0afd6f1cab
Lee Saville <l.w.saville@gmail.com>
2016-07-31 15:57:46 -0600

    Add ability to handle whole file, include samples

--- encryptor.rb
+++ encryptor.rb
@@ -30,4 +30,38 @@ class Encryptor
   end
     results.join
   end
+
+  def encrypt_file(filename, rotation)
+    #create the file handle to the input file
+    input = File.open(filename, 'r')
+    #read the text of the input file
+    content = input.read
+    #encrypt the text
+    encrypted_content = encrypt(content, rotation)
+    #create a name for the output file
+    encrypted_file = filename + '.encrypted'
+    #create an output file handle
+    out = File.open(encrypted_file, 'w')
+    #write out the text
+    out.write(encrypted_content)
+    #close the file
+    out.close
+  end
+
+  def decrypt_file(filename, rotation)
+    #create the file handle to the encrypted file
+    input = File.open(filename, 'r')
+    #read the encrypted text
+    contents = input.read
+    #decrypt the text by passing in the text and rotation
+    decrypted_content = decrypt(contents, rotation)
+    #create a name for the decrypted file
+    decrypted_file = filename.gsub(/encrypted/, "decrypted")
+    #create an output file handle
+    out = File.open(decrypted_file, 'w')
+    #write out the text
+    out.write(decrypted_content)
+    #close the file
+    out.close
+  end
 end
